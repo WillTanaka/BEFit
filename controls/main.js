8ab32d581de29789183345582138df2b2b716105
WillTanaka <williamt@alunos.utfpr.edu.br>
2024-04-17 19:38:16 -0300

    Desenvolvimento tela iniciale e tela login

--- controls/main.js
+++ controls/main.js
@@ -5,4 +5,21 @@ router.get("/", (req, res) => {
     res.render("index")
 })
 
+router.get("/login", (req, res) => {
+    res.render("login")
+});
+
+router.get("/pages", (req, res) => {
+    res.render("pages")
+});
+
+router.get("/home", (req, res) => {
+    res.render("home")
+});
+
+router.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/login");
+});
+
 module.exports = router

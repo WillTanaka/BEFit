0d662972e61bcea83e05684a94cd20fe47369943
WillTanaka <williamt@alunos.utfpr.edu.br>
2024-04-17 21:49:43 -0300

    Desenvolvimento funcionalidade de login

--- controls/main.js
+++ controls/main.js
@@ -5,21 +5,34 @@ router.get("/", (req, res) => {
     res.render("index")
 })
 
-router.get("/login", (req, res) => {
-    res.render("login")
-});
-
-router.get("/pages", (req, res) => {
-    res.render("pages")
-});
-
 router.get("/home", (req, res) => {
     res.render("home")
 });
 
-router.get("/logout", (req, res) => {
+// Rota para exibir a página de login
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Rota para processar o formulário de login
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+        req.session.user = username;
+        res.redirect('/home');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+// Rota para fazer logout
+router.get('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect("/login");
+    res.redirect('/login');
+});
+
+router.get("/pages", (req, res) => {
+    res.render("pages")
 });
 
 module.exports = router

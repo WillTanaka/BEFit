20503c8977b2c514cb2abfff3042d6a51a0ec4c0
pedrohenriquesf <pedrofernandespedro36@gmail.com>
2024-04-22 16:55:11 -0300

    Validação Joi
    Contribuindo com as validações do projeto

--- validate.js
+++ validate.js
@@ -1,18 +1,57 @@
 const Joi = require('joi');
-require('dotenv').config();
 
-// Define o esquema de validação usando Joi
-const schema = Joi.object({
-  USER_NAME: Joi.string().required(),
-  PASSWORD: Joi.string().required(),
+// Definir os esquemas de validação
+const loginSchema = Joi.object({
+  user_name: Joi.string().required(),
+  password: Joi.string().required()
 });
 
-// Valida os valores do arquivo .env
-const { error, value } = schema.validate(process.env);
+const pageSchema = Joi.object({
+  url: Joi.string().required(),
+  content: Joi.string().required()
+});
 
-if (error) {
-  console.error('Erro de validação:', error.message);
-  process.exit(1);
+const editPageSchema = Joi.object({
+  content: Joi.string().required()
+});
+
+// Funções para validar os parâmetros
+function validarLogin(req, res, next) {
+  const { error } = loginSchema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  } else {
+    next();
+  }
 }
 
-console.log('Arquivo .env válido:', value);
+function validarPage(req, res, next) {
+  const { error } = pageSchema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  } else {
+    next();
+  }
+}
+
+function validarEditPage(req, res, next) {
+  const { error } = editPageSchema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+  } else {
+    next();
+  }
+}
+
+// Usar as funções como middleware em suas rotas
+router.post('/login', validarLogin, (req, res) => {
+  // Seu código aqui
+});
+
+router.post('/pages', validarPage, (req, res) => {
+  // Seu código aqui
+});
+
+router.post('/:url/edit', isAuthenticated, validarEditPage, (req, res) => {
+  // Seu código aqui
+});

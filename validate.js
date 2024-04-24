597a69c8e71ff26435b889666d851d9f76e0b42f
pedrohenriquesf <pedrofernandespedro36@gmail.com>
2024-04-24 20:00:51 -0300

    Joi
    foi feito a validação de usuário

--- validate.js
+++ validate.js
@@ -1,2 +1,11 @@
 const Joi = require('joi');
 
+// Esquema Joi para a rota de login
+const loginSchema = Joi.object({
+    user_name: Joi.string().required(),
+    password: Joi.string().required()
+});
+
+// Esquema Joi para a rota de criação de páginas
+const pageSchema = Joi
+

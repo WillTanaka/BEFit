100934afe6f4c3c347d00893021280def349b915
pedrohenriquesf <pedrofernandespedro36@gmail.com>
2024-04-26 15:35:58 -0300

    Nova ajuste do file validate
    Foi redefinido o Schema de login e pages .

--- validate.js
+++ validate.js
@@ -1,11 +1,11 @@
 const Joi = require('joi');
 
-// Esquema Joi para a rota de login
 const loginSchema = Joi.object({
     user_name: Joi.string().required(),
     password: Joi.string().required()
 });
 
-// Esquema Joi para a rota de criação de páginas
-const pageSchema = Joi
-
+const pageSchema = Joi.object({
+    url: Joi.string().required(),
+    content: Joi.string().required()
+});

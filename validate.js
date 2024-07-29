6bd5880e89b9521e62f8124b1c09df7b7e2348d6
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2024-07-29 00:49:27 -0300

    validação

--- validate.js
+++ validate.js
@@ -1,11 +1,33 @@
 const Joi = require('joi');
 
+//Definição do esquema de validação para login
 const loginSchema = Joi.object({
     user_name: Joi.string().required(),
     password: Joi.string().required()
 });
 
+//Definição do esquema de validação para página
 const pageSchema = Joi.object({
     url: Joi.string().required(),
     content: Joi.string().required()
 });
+
+module.exports = {
+    validateLogin: (req, res, next) => {
+        //Valida o corpo da requisição de login
+        const { error } = loginSchema.validate(req.body);
+        if (error) {
+            //Retorna uma mensagem de erro se a validação falhar
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        next();
+    },
+    validatePage: (req, res, next) => {
+        //Valida o corpo da requisição de página
+        const { error } = pageSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        next();
+    }
+};
